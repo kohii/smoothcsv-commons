09898b027ea427ca57969ea5773d1a53a6555c70
kohii <kohii.git@gmail.com>
2015-12-31 21:13:12 +0900

    Change toString's behavior

--- src/main/java/com/smoothcsv/commons/utils/ObjectUtils.java
+++ src/main/java/com/smoothcsv/commons/utils/ObjectUtils.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2015 kohii
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,7 +14,7 @@
 package com.smoothcsv.commons.utils;
 
 /**
- * 
+ *
  * @author kohii
  */
 public class ObjectUtils {
@@ -24,6 +24,6 @@ public class ObjectUtils {
   }
 
   public static String toString(Object object) {
-    return object == null ? "" : object.toString();
+    return object == null ? null : object.toString();
   }
 }
